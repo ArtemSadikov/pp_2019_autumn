cd3960c0dfc885625c5ac55f71c2e50f6cdfe63a
ArtemSadikov <marolboron@ya.ru>
2019-11-15 00:25:16 +0300

    VECTOR DON'T FORGET

---
+++ modules/task_2/sadikov_a_simple_method/main.cpp
@@ -0,0 +1,123 @@
+// Copyright 2019 Sadikov Artem
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./simple_method.h"
+
+TEST(Simple_Method_Slae, Test_On_Matrix_Size_3) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<double> matrix(12);
+    if (rank == 0) {
+        matrix[0] = 4;
+        matrix[1] = 1;
+        matrix[2] = 1;
+        matrix[3] = 9;
+        matrix[4] = 1;
+        matrix[5] = 6;
+        matrix[6] = -1;
+        matrix[7] = 10;
+        matrix[8] = 1;
+        matrix[9] = 2;
+        matrix[10] = 5;
+        matrix[11] = 20;
+    }
+
+    std::vector<double> actual(3);
+
+    actual = get_res(matrix, 3, 0.22);
+
+    if (rank == 0) {
+        std::vector<double> expected(3);
+        expected[0] = 1.03958;
+        expected[1] = 2.00833;
+        expected[2] = 3.05;
+        EXPECT_TRUE(is_equal(actual, expected));
+    }
+}
+
+TEST(Simple_Method_Slae, Cant_Get_Res_With_Wrong_Size) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<double> matrix;
+
+    if (rank == 0) {
+        matrix = get_rand_matrix(4);
+    }
+    if (rank == 0) {
+        std::vector<double> act(4);
+        ASSERT_ANY_THROW(act = get_res(matrix, 2, 0.001));
+    }
+}
+
+TEST(Simple_Method_Slae, Test_On_Matrix_Size_4) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    std::vector<double> matrix(20);
+    if (rank == 0) {
+        matrix[0] = 10;
+        matrix[1] = -1;
+        matrix[2] = 2;
+        matrix[3] = 0;
+        matrix[4] = 6;
+        matrix[5] = -1;
+        matrix[6] = 11;
+        matrix[7] = -1;
+        matrix[8] = 3;
+        matrix[9] = 25;
+        matrix[10] = 2;
+        matrix[11] = -1;
+        matrix[12] = 10;
+        matrix[13] = -1;
+        matrix[14] = -11;
+        matrix[15] = 0;
+        matrix[16] = 3;
+        matrix[17] = -1;
+        matrix[18] = 8;
+        matrix[19] = 15;
+    }
+
+    std::vector<double> actual(4);
+
+    actual = get_res(matrix, 4, 0.99);
+
+    std::vector<double> expected(4);
+    if (rank == 0) {
+        expected[0] = 1.04727;
+        expected[1] = 1.7159;
+        expected[2] = -0.80522;
+        expected[3] = 0.88522;
+
+        EXPECT_TRUE(is_equal(actual, expected));
+    }
+}
+
+TEST(Simple_Method_Slae, Not_Create_Matrix_If_Size_Less_Then_2) {
+    ASSERT_ANY_THROW(std::vector<double> matrix = get_rand_matrix(1));
+}
+
+TEST(Simple_Method_Slae, Can_Create_Matrix_On_5_Size) {
+    ASSERT_NO_THROW(std::vector<double> matrix = get_rand_matrix(5));
+}
+
+TEST(Simple_Method_Slae, Can_Create_Matrix_On_10_Size) {
+    ASSERT_NO_THROW(std::vector<double> matrix = get_rand_matrix(10));
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_2/sadikov_a_simple_method/simple_method.cpp
@@ -0,0 +1,148 @@
+// Copyright 2019 Sadikov Artem
+#include <mpi.h>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <cmath>
+#include <limits>
+#include "../../../modules/task_2/sadikov_a_simple_method/simple_method.h"
+
+std::vector<double> get_rand_matrix(int size) {
+    if (size < 2)
+        throw "ERR";
+
+    std::mt19937 gen;
+    gen.seed(static_cast<double>(time(NULL)));
+
+    std::vector<double> matrix(size * (size + 1));
+    for (int i = 0; i < size * (size + 1); i++) {
+        matrix[i] = gen() % 20;
+    }
+
+    return matrix;
+}
+
+bool is_equal(std::vector<double> x, std::vector<double> y) {
+    for (int i = 0; i < static_cast<int>(x.size()); i++) {
+        if (!(std::fabs(x[i] - y[i]) < 1e-4))
+            return false;
+    }
+    return true;
+}
+
+std::vector<double> solve_simple(std::vector<double> delta_a, std::vector<double> x,
+                 double error, int size, int rank, int row_count,
+                 int size_proc) {
+    std::vector<double> x_old;
+    int iter = 0, core;
+    double norm = 0;
+    std::vector<int> sendcounts, displs;
+
+    sendcounts.resize(size_proc);
+    displs.resize(size_proc);
+
+    MPI_Scan(&row_count, &core, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    core -= row_count;
+
+    MPI_Allgather(&row_count, 1, MPI_INT, &sendcounts[0], 1, MPI_INT,
+                 MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    displs[0] = 0;
+    for (int i = 1; i < size_proc; i++) {
+        displs[i] = displs[i - 1] + sendcounts[i - 1];
+    }
+
+    x_old.resize(size);
+
+    do {
+        iter++;
+        norm = x[0];
+
+        for (int i = 0; i < size; i++) { x_old[i] = x[i]; }
+
+        double sum;
+
+        for (int i = 0; i < row_count; i++) {
+            sum = 0.0;
+            for (int j = 0; j < i + core; j++) {
+                sum += delta_a[i * (size + 1) + j] * x_old[j];
+            }
+
+            for (int j = 1 + core + i; j < size; j++) {
+                sum += delta_a[i * (size + 1) + j] * x_old[j];
+            }
+
+            x[i + core] = static_cast<double>(delta_a[i * (size + 1) + size]
+                         - sum) /
+                         static_cast<double>(delta_a[i * (size + 1) + i + core]);
+        }
+
+        MPI_Allgatherv(&x[0] + core, row_count, MPI_DOUBLE,
+                        &x[0], &sendcounts[0], &displs[0], MPI_DOUBLE,
+                        MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            std::vector<double> val(size);
+            for (int i = 0; i < size; i++) {
+                val[i] = fabs(x[i] - x_old[i]);
+            }
+            double mx = val[0];
+            for (int i = 0; i < size; i++) {
+                if (mx < val[i])
+                    mx = val[i];
+            }
+            norm = mx;
+        }
+
+        MPI_Bcast(&norm, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    } while (error < norm);
+
+    return x;
+}
+
+std::vector<double> get_res(std::vector<double> matrix, int size, double error) {
+    if (size * (size + 1) != static_cast<int>(matrix.size()))
+        throw "WRONG";
+
+    int row_count, SIZE, size_proc, rank;
+    std::vector<double> x;
+    std::vector<double> delta_a;
+    std::vector<int> sendcounts, displs;
+    MPI_Comm_size(MPI_COMM_WORLD, &size_proc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&error, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    x.resize(size);
+    if (rank == 0) {
+        for (int i = 0; i < size; i++) { x[i] = 0.0; }
+    }
+    MPI_Bcast(&x[0], size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    row_count = (size / size_proc) +
+                ((size % size_proc) > rank ? 1 : 0);
+
+    delta_a.resize((size + 1) * row_count);
+    displs.resize(size_proc);
+    sendcounts.resize(size_proc);
+
+    SIZE = (size + 1) * row_count;
+
+    MPI_Gather(&SIZE, 1, MPI_INT, &sendcounts[0], 1, MPI_INT, 0,
+                MPI_COMM_WORLD);
+
+    displs[0] = 0;
+    for (int i = 1; i < size_proc; i++) {
+        displs[i] = displs[i - 1] + sendcounts[i - 1];
+    }
+
+    MPI_Scatterv(&matrix[0], &sendcounts[0], &displs[0], MPI_DOUBLE,
+                 &delta_a[0], SIZE, MPI_DOUBLE,
+                 0, MPI_COMM_WORLD);
+
+    x = solve_simple(delta_a, x, error, size, rank, row_count, size_proc);
+
+    return x;
+}
